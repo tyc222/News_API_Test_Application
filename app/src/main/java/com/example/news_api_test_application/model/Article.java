b519f84c8afa940ce093bc50473adf3d13153005
Tim <>
2019-04-24 09:15:51 +0800

    Create Source Class

--- app/src/main/java/com/example/news_api_test_application/model/Article.java
+++ app/src/main/java/com/example/news_api_test_application/model/Article.java
@@ -4,9 +4,8 @@ import android.net.Uri;
 
 import com.google.gson.annotations.SerializedName;
 
-import retrofit2.http.Url;
 
-public class Articles {
+public class Article {
 
     @SerializedName("author")
     private String author;
@@ -15,13 +14,77 @@ public class Articles {
     @SerializedName("description")
     private String description;
     @SerializedName("url")
-    private Url url;
+    private String url;
     @SerializedName("urlToImage")
-    private Url urlToImage;
+    private String urlToImage;
     @SerializedName("publishedAt")
     private String publishedAt;
     @SerializedName("content")
     private String content;
 
-    
+    public Article (String author, String title, String description, String url, String urlToImage, String publishedAt, String content) {
+        this.author = author;
+        this.title = title;
+        this.description = description;
+        this.url = url;
+        this.urlToImage = urlToImage;
+        this.publishedAt = publishedAt;
+        this.content = content;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getUrlToImage() {
+        return urlToImage;
+    }
+
+    public void setUrlToImage(String urlToImage) {
+        this.urlToImage = urlToImage;
+    }
+
+    public String getPublishedAt() {
+        return publishedAt;
+    }
+
+    public void setPublishedAt(String publishedAt) {
+        this.publishedAt = publishedAt;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
 }
