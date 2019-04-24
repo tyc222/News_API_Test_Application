43184f60528eb921216e4fda0fcd69490c592ad5
Tim <>
2019-04-24 10:03:31 +0800

    Add parameters to GetNewsDataService Interface

--- app/src/main/java/com/example/news_api_test_application/network/GetNewsDataService.java
+++ app/src/main/java/com/example/news_api_test_application/network/GetNewsDataService.java
@@ -1,9 +1,12 @@
 package com.example.news_api_test_application.network;
 
+import com.example.news_api_test_application.model.ArticleList;
+
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Query;
 
 public interface GetNewsDataService {
     @GET("top-headlines")
-    Call<>
+    Call<ArticleList> getArticleData(@Query("country=au" + "&" + "apiKey=30f23670bbb5441bbd9e77746df08fd4") int countryNo);
 }
