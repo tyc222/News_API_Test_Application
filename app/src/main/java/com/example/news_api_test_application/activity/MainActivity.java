7ee486caa5df6013734935bf701bbd1b6efca9ad
Tim <>
2019-04-25 10:52:57 +0800

    Set up FAB

--- app/src/main/java/com/example/news_api_test_application/activity/MainActivity.java
+++ app/src/main/java/com/example/news_api_test_application/activity/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.news_api_test_application.activity;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
 
 import com.example.news_api_test_application.R;
@@ -25,11 +27,63 @@ public class MainActivity extends AppCompatActivity {
     private ArticleAdapter adapter;
     private RecyclerView recyclerView;
     private String country;
+    FloatingActionButton fabAu, fabTw, fabUK, fabNZ, fabUS;
+    // Boolean to determine if our FAB is pressed
+    private boolean isFABOpen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Create a FAB menu
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fabAu = findViewById(R.id.fabAu);
+        fabTw = findViewById(R.id.fabTw);
+        fabUK = findViewById(R.id.fabUk);
+        fabNZ = findViewById(R.id.fabNz);
+        fabUS = findViewById(R.id.fabUs);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!isFABOpen){
+                    showFABMenu();
+                } else {
+                    closeFABMenu();
+                }
+            }
+        });
+        fabAu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                country = "au";
+            }
+        });
+        fabTw.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                country = "tw";
+            }
+        });
+        fabUK.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                country = "uk";
+            }
+        });
+        fabNZ.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                country = "nz";
+            }
+        });
+        fabUS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                country = "us";
+            }
+        });
+
 
         // Create handle for the RetrofitInstance interface
         GetNewsDataService service = RetrofitInstance.getRetrofitInstance().create(GetNewsDataService.class);
@@ -48,8 +102,6 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onResponse(Call<ArticleList> call, Response<ArticleList> response) {
                 generateNewsList(response.body().getArticleList());
-
-
             }
 
             @Override
@@ -68,5 +120,25 @@ public class MainActivity extends AppCompatActivity {
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setAdapter(adapter);
+    }
+
+    // To open FAB menu
+    private void showFABMenu(){
+        isFABOpen =true;
+        fabAu.animate().translationY(-getResources().getDimension(R.dimen.standard_60));
+        fabTw.animate().translationY(-getResources().getDimension(R.dimen.standard_120));
+        fabUK.animate().translationY(-getResources().getDimension(R.dimen.standard_180));
+        fabNZ.animate().translationY(-getResources().getDimension(R.dimen.standard_240));
+        fabUS.animate().translationY(-getResources().getDimension(R.dimen.standard_300));
+    }
+
+    // To close FAB menu
+    private void closeFABMenu(){
+        isFABOpen=false;
+        fabAu.animate().translationY(0);
+        fabTw.animate().translationY(0);
+        fabUK.animate().translationY(0);
+        fabNZ.animate().translationY(0);
+        fabUS.animate().translationY(0);
     }
 }
